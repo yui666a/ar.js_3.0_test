f7cc1b92714752301968226337fc39f4f4c91d77
yui666a <haiso666@gmail.com>
2021-05-08 18:35:50 +0900

    raycaster色々追加

--- meisi_aiso_script.js
+++ meisi_aiso_script.js
@@ -47,12 +47,29 @@
 //     console.log("contact clicked");
 // });
 
-var targetEl = document.querySelector("#target");
-targetEl.addEventListener("click", function () {
-    targetEl.setAttribute("material", {
-        color: "red"
-    });
-    console.log("clicked");
+// var targetEl = document.querySelector("#target");
+// targetEl.addEventListener("click", function () {
+//     targetEl.setAttribute("material", {
+//         color: "red"
+//     });
+//     console.log("clicked");
+// });
+
+AFRAME.registerComponent("cursor-listener", {
+    init: function () {
+        let self = this;
+        this.el.addEventListener("click", function (e) {
+            self.el.setAttribute("color", "#f0f");
+        }, false);
+
+        this.el.addEventListener("raycaster-intersected", function (e) {
+            self.el.setAttribute("color", "#00f");
+        }, false);
+
+        this.el.addEventListener("raycaster-intersected-cleared", function (e) {
+            self.el.setAttribute("color", "#fff");
+        }, false);
+    }
 });
 
 // AFRAME.registerComponent("cursor-listener", {
