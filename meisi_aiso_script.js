6760d165c8e099f92adbe722156a3142f77bcf71
yui666a <haiso666@gmail.com>
2021-05-08 19:24:00 +0900

    大きさ

--- meisi_aiso_script.js
+++ meisi_aiso_script.js
@@ -55,22 +55,22 @@ targetEl.addEventListener("click", function () {
     console.log("clicked");
 });
 
-AFRAME.registerComponent("cursor-listener", {
-    init: function () {
-        let self = this;
-        this.el.addEventListener("click", function (e) {
-            self.el.setAttribute("color", "#f0f");
-        }, false);
+// AFRAME.registerComponent("cursor-listener", {
+//     init: function () {
+//         let self = this;
+//         this.el.addEventListener("click", function (e) {
+//             self.el.setAttribute("color", "#f0f");
+//         }, false);
 
-        this.el.addEventListener("raycaster-intersected", function (e) {
-            self.el.setAttribute("color", "#00f");
-        }, false);
+//         this.el.addEventListener("raycaster-intersected", function (e) {
+//             self.el.setAttribute("color", "#00f");
+//         }, false);
 
-        this.el.addEventListener("raycaster-intersected-cleared", function (e) {
-            self.el.setAttribute("color", "#fff");
-        }, false);
-    }
-});
+//         this.el.addEventListener("raycaster-intersected-cleared", function (e) {
+//             self.el.setAttribute("color", "#fff");
+//         }, false);
+//     }
+// });
 
 // AFRAME.registerComponent("cursor-listener", {
 //     init: function () {
